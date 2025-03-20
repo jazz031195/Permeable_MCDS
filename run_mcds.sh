b079b6c0401b99d439e7a1f2ae7dd96ba46fa98a
Rita Oliveira <o_anarita@hotmail.com>
2025-03-20 15:17:04 +0100

    put back a default path to the simulator

--- run_mcds.sh
+++ run_mcds.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -l
 ./compile.sh
-./MC-DC_Simulator "/home/localadmin/Documents/Rita/Codes_GitHub/Simulator_WM/Simulator_GMWM/instructions/conf/model_test.conf"
+./MC-DC_Simulator "path/to/conf/file"
