674c9d79dd34701dbaf19238af279af4ffeb886e
ideriedm <inesderied@hotmail.ch>
2025-04-22 09:26:32 +0200

    swelling

--- create_swelling_swc.py
+++ create_swelling_swc.py
@@ -16,16 +16,15 @@ def shrink_swc(path, new_path, shrink_perc):
         lines = lines[2:]
         for i in range(len(lines)):
             coords = lines[i].split(' ')
-            if len(coords) > 2 and len(coords) < 6:
-                x = coords[0] 
-                y = coords[1] 
-                z = coords[2]
-                r = float(coords[3])
+            if len(coords) > 2:
+                x = coords[3] 
+                y = coords[4] 
+                z = coords[5]
+                r = float(coords[6])
                 new_r = shrink_radius(shrink_perc, r)
-                outfile.write(f'{x} {y} {z} {new_r}\n')
+                outfile.write(f'{coords[0]} {coords[1]} {coords[2]} {x} {y} {z} {new_r}\n')
             else:
                 outfile.write(lines[i])
-
 
     
 def shrink_radius(perc, swollen_radius):
@@ -37,8 +36,8 @@ def shrink_radius(perc, swollen_radius):
 if __name__ == "__main__":
 
     # shrink by 1%
-    path = "results/Simu/neurons_list_49_perc.txt"
-    new_path = "results/Simu/neurons_list_49_perc_1_perc_shrink.txt"
+    path = "results/Simu/neurons_list_22_5_perc.txt"
+    new_path = "results/Simu/neurons_list_22_5_perc_shrink.txt"
     shrink_perc = 0.01
     new_lines = shrink_swc(path, new_path, shrink_perc)
 
