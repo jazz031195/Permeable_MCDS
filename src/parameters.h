745cc65776eb9eaa27b96efa9d4599d7d94a23da
jazz031195 <jasmine.nguyenduc@gmail.com>
2025-09-01 09:04:54 +0200

    noooow it works

--- src/parameters.h
+++ src/parameters.h
@@ -30,6 +30,7 @@ public:
     double sim_duration;                            /*!< simulation total time                                                      */
     bool write_traj;                                /*!< flag, write a traj file or not, binary format only                         */
     bool write_hit;                                 /*!< flag, write a hit file or not, binary format only                         */
+    bool write_location;                            /*!< flag, write the location of the walker at each time step                   */
     unsigned write_every_nth_step = 1; /*!< write every Nth step to scale down trajectory to relevant time scale for dMRI */
     double concentration;                           /*< concentration of walkers per mm³ */
     bool write_full_c;
--- src/trajectory.cpp
+++ src/trajectory.cpp
@@ -672,6 +672,9 @@ void Trajectory::closeHitReaderFile()
 
 void Trajectory::setTrajFile(std::string trajfile_)
 {
+    if (write_location){
+        trajfile_ = trajfile_ + "_location";
+    }
     trajfile = trajfile_  + ".traj";
     headerfile = trajfile_+ ".bhdr";
     readTrajectoryHeader();
